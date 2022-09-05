596f5a640c657ac13900c8bde8fcc898ed539756
Mine Ceyhan <61575499+mineceyhan@users.noreply.github.com>
2022-09-05 22:27:48 +0300

    Update header.blade.php

--- resources/views/front/layouts/header.blade.php
+++ resources/views/front/layouts/header.blade.php
@@ -30,7 +30,6 @@
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ms-auto py-4 py-lg-0">
                         <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="{{url('/blog')}}">Anasayfa</a></li>
-                        <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="{{url('/random')}}">Random Blog</a></li>
                         <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="{{url('/loginPage')}}">Blog Paylaş</a></li>
                     </ul>
                 </div>
