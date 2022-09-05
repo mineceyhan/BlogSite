74323da1b9befa5d1bc115dcffcdf3eb9bdb015e
Mine Ceyhan <61575499+mineceyhan@users.noreply.github.com>
2022-09-05 22:24:15 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,16 @@ use App\Http\Controllers\AuthController;
 */
 Route::get('/panel' , [DashboardController::class, 'index'])->name('panel');
 Route::get('/update/{id}' , [DashboardController::class, 'update']);
-Route::post('/updatePost' , [DashboardController::class, 'updatePost'])->name('updatePost');
 Route::get('/insertPage' , [DashboardController::class, 'insertPage'])->name('insertPage');
-Route::post('/insert' , [DashboardController::class, 'insert'])->name('insert');
 Route::get('/delete/{id}' , [DashboardController::class, 'delete']);
 Route::get('category1/{id}', [DashboardController::class, 'category']);
 Route::view('/registerPage' , 'auth/register');
 Route::view('/loginPage' , 'auth/login');
+
+Route::middleware(['throttle:global'])->group(function () {
+    Route::post('/updatePost' , [DashboardController::class, 'updatePost'])->name('updatePost');
+    Route::post('/insert' , [DashboardController::class, 'insert'])->name('insert');
+});
 
 // Front Routes
 Route::resource('/blog', HomepageController::class);
@@ -27,7 +30,6 @@ Route::get('/random', [HomepageController::class , 'randomBlog']);
 Route::get('category/{id}', [CategoryController::class, 'index']);
 
 //Auth Routes
-
 
 Route::get('/logout' , function(){
     if( session()->has('id')){
